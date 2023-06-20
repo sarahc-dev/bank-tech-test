756554d7d009709dbf16e201decaa7986fa39683
sarahc-dev <sarahclements@hotmail.co.uk>
2023-06-20 11:47:42 +0100

    add test for multiple deposit transactions. Passing green

--- src/bank.integration.test.js
+++ src/bank.integration.test.js
@@ -12,4 +12,26 @@ describe("bank integration", () => {
         account.addTransaction(deposit);
         expect(account.getTransactions()).toEqual([{ date: "10-01-2023", credit: 1000, balance: 1000 }]);
     });
+
+    it("makes multiple deposit transactions", () => {
+        const account = new Account();
+        const transaction = new Transaction();
+
+        const balance = account.getBalance();
+        const date = new Date("2023-01-10");
+        const deposit = transaction.deposit(date, 1000, balance);
+        account.addTransaction(deposit);
+
+        const newBalance = account.getBalance();
+        const secondDate = new Date("2023-01-13");
+        const secondDeposit = transaction.deposit(secondDate, 2000, newBalance);
+        account.addTransaction(secondDeposit);
+
+        expect(account.getTransactions().length).toBe(2);
+        expect(account.getBalance()).toBe(3000);
+        expect(account.getTransactions()).toEqual([
+            { date: "10-01-2023", credit: 1000, balance: 1000 },
+            { date: "13-01-2023", credit: 2000, balance: 3000 },
+        ]);
+    });
 });
